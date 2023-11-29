1bdf22a9393fb54ed0677728c42f6385d348db65
firdigunawan <firdigunawan10@gmail.com>
2023-11-29 19:03:23 +0700

    perubahan

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ function hitungTotalbromo() {
 
 
 
-// Fungsi untuk menghitung total harga Di bromo
+// Fungsi untuk menghitung total harga Di labuanbajo
 function hitungTotallabuanbajo() {
   // Mendapatkan nilai jumlah pembelian dari input
   var quantity = document.getElementById('quantityInput-labuanbajo').value;
@@ -69,4 +69,73 @@ function hitungTotallabuanbajo() {
   // Menampilkan total harga pada span dengan id 'totalHarga'
   //totalHarga.toLocaleString('id-ID'); digunakan untuk mengubah nilai totalHarga menjadi format string dengan pemisah ribuan dan menggunakan tanda desimal sesuai dengan konvensi bahasa Indonesia ('id-ID').
   document.getElementById('totalHarga-labuanbajo').innerText = totalHarga.toLocaleString('id-ID');
+}
+
+
+// Fungsi untuk menampilkan hasil pencarian berdasarkan kata  yang dimasukkan oleh pengguna
+function showResults(searchTerm) {
+  // Mendapatkan elemen kontainer hasil pencarian
+  const resultsContainer = document.getElementById("search-results");
+  // Mendapatkan elemen hasil pencarian spesifik
+  const baliResults = document.getElementById("baliResults");
+  const bromoResults = document.getElementById("bromoResults");
+  const labuanResults = document.getElementById("labuanResults");
+  
+  // Mendapatkan elemen hasil pencarian yang tidak dikenal
+  const uknownResults = document.getElementById("uknownResults");
+
+  // Menghapus hasil pencarian sebelumnya
+  resultsContainer.innerHTML = "";
+
+  // Memeriksa apakah kata pencarian tidak kosong
+  if (searchTerm.trim() !== "") {
+    // Memeriksa apakah kata pencarian adalah "bali"
+    if (searchTerm.toLowerCase() === "bali") {
+      // Menampilkan hasil pencarian untuk "bali"
+      baliResults.style.display = "block";
+      
+      // Menyembunyikan hasil pencarian yang tidak dikenal
+      uknownResults.style.display = "none";
+    } 
+    // Memeriksa apakah kata pencarian adalah "labuan"
+    else if (searchTerm.toLowerCase() === "labuan") {
+      // Menampilkan hasil pencarian untuk "labuan"
+      labuanResults.style.display = "block";
+      
+      // Menyembunyikan hasil pencarian yang tidak dikenal
+      uknownResults.style.display = "none";
+    }
+    // Memeriksa apakah kata pencarian adalah "bromo"
+    else if (searchTerm.toLowerCase() === "bromo") {
+      // Menampilkan hasil pencarian untuk "bromo"
+      bromoResults.style.display = "block";
+      
+      // Menyembunyikan hasil pencarian yang tidak dikenal
+      uknownResults.style.display = "none";
+    }
+    // Jika kata pencarian tidak sesuai dengan kriteria di atas
+    else {
+      // Menampilkan hasil pencarian yang tidak dikenal
+      uknownResults.style.display = "block";
+      
+      // Menyembunyikan hasil pencarian spesifik lainnya
+      baliResults.style.display = "none";
+      bromoResults.style.display = "none";
+      labuanResults.style.display = "none";
+    }
+
+    // Menampilkan kontainer hasil pencarian secara keseluruhan
+    resultsContainer.style.display = "block";
+  } 
+  // Jika kata pencarian kosong
+  else {
+    // Menyembunyikan semua hasil pencarian
+    baliResults.style.display = "none";
+    bromoResults.style.display = "none";
+    labuanResults.style.display = "none";
+    uknownResults.style.display = "none";
+    
+    // Menyembunyikan kontainer hasil pencarian secara keseluruhan
+    resultsContainer.style.display = "none";
+  }
 }
